8176b3d12cc300cdae4c15d1877d770f43e9143b
curtcurt69 <cecrouse96@yahoo.com>
2024-03-10 01:44:19 -0500

    Creates basic frontedn for shopping app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { Outlet } from 'react-router-dom'
 
 const App = () => {
   return (
-    <h1>Welcome to the Proshop</h1>
+    <>
+      <Header />
+      <main>
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+    </>
   )
 }
 
