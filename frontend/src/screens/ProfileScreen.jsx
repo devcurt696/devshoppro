ffe913dea2a0de54507ce3af12a8fad85bdd9bad
curtcurt69 <cecrouse96@yahoo.com>
2024-03-22 04:38:18 -0400

    Minor bug fix

--- frontend/src/screens/ProfileScreen.jsx
+++ frontend/src/screens/ProfileScreen.jsx
@@ -128,7 +128,7 @@ const ProfileScreen = () => {
                                 </td>
                                 <td>
                                     { order.isDelivered ? (
-                                        order.isDelivered.substring(0,10)
+                                        order.deliveredAt.substring(0, 10)
                                     ) : (
                                         <FaTimes style={{ color: 'red' }} />
                                     )}
