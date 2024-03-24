1772be1f247c8ebdd669f13a00fb42364adee173
curtcurt69 <cecrouse96@yahoo.com>
2024-03-24 19:02:27 -0400

    update copyright

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <Container>
             <Row>
                 <Col className='text-center py-3'>
-                    <p>ProShop &copy; {currYear}</p>
+                    <p>DevShop.Pro &copy; {currYear}</p>
                 </Col>
             </Row>
         </Container>
